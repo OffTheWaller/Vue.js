9ccc4c7b2c08bd424b879516700278f59735f743
OffTheWaller <1031118178@qq.com>
2018-09-10 16:11:10 +0800

    分析tab组件

--- code/组件/tab栏组件/tabs.js
+++ code/组件/tab栏组件/tabs.js
@@ -26,10 +26,12 @@ Vue.component('tabs', {
             return [
                 'tabs-tab',
                 {
+                    //给当前选中的tab加一个class
                     'tabs-tab-active': item.name === this.currentValue
                 }
             ]
         },
+        //获取所有pane组件实例
         getTabs () {
             return this.$children.filter(function (item) {
                 return item.$options.name === 'pane';
@@ -38,14 +40,17 @@ Vue.component('tabs', {
         updateNav () {
             this.navList = [];
             var _this = this;
+            //拿navList
             this.getTabs().forEach(function (pane, index) {
                 _this.navList.push({
                     label: pane.label,
                     name: pane.name || index
                 });
+                //如果没有给pane设置name，默认设置为index
                 if (!pane.name) {
                     pane.name = index;
                 }
+                //设置当前选中的tab的索引
                 if (index === 0) {
                     if (!_this.currentValue) {
                         _this.currentValue = pane.name || index;
@@ -57,14 +62,18 @@ Vue.component('tabs', {
         updateStatus () {
             var tabs = this.getTabs();
             var _this = this;
+            //将当前选中的tab对应的pane显示
             tabs.forEach(function (tab) {
                 return tab.show = tab.name === _this.currentValue;
             }) 
         },
+        //点击tab标题时触发
         handleChange: function (index) {
             var nav = this.navList[index];
             var name = nav.name;
+            //改变当前选中的tab,并触发下面的watch
             this.currentValue = name;
+            //更新value
             this.$emit('input', name);
             this.$emit('on-click', name);
         }
